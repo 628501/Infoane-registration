051cfd2ffbb67bd4d51f78f9a21951cbb6b11973
Kartheeswaran_16 <karthis15cse@gmail.com>
2025-04-10 19:48:54 +0530

    Removed Unwanted

--- Frontend/src/api/axiosInstance.tsx
+++ Frontend/src/api/axiosInstance.tsx
@@ -49,11 +49,4 @@ axiosInstance.interceptors.response.use(
   }
 );
 
-export function getCookie(name: string): string | null {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop()?.split(";").shift() || null;
-  return null;
-}
-
 export default axiosInstance;
